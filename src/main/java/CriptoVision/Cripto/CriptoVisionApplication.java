9cd329870cd79619a3cc3b59fea4b3f75cbabefd
Molina <elison_li@hotmail.com>
2024-01-24 15:42:13 -0300

    Estrutura do Projeto Cripto

---
+++ src/main/java/CriptoVision/Cripto/CriptoVisionApplication.java
@@ -0,0 +1,14 @@
+package CriptoVision.Cripto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoVisionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CriptoVisionApplication.class, args);
+
+		System.out.println("Teste");
+    }
+
+}
